2bbd706c733aefbba186285eee840fa0bf9269e0
Taylor Pennock <taylor.pennock@gmail.com>
2017-01-12 21:02:42 -0700

    updating style to use the old header;

--- about.php
+++ about.php
@@ -21,7 +21,7 @@
 					<img src="images/lvac/about_bg.jpg">
 				</div>
 				<div class="team-desc col-md-5">
-					<div class="team-title"><h4>Dr. Sid Carter</h4></div>
+					<div class="team-title"><h4>Dr. Sidney Carter, D.C.</h4></div>
 					<div class="team-content">
 						<p>Dr. Carter's longtime love of animals encouraged him to pursue a degree in biology from UNLV. He went on to further study at Los Angeles College of Chiropractic where he earned the degree of Chiropractic Physician. With his ongoing interest in animal wellness, Dr. Carter received post graduate training in Animal Chiropractic at Parker Chiropractic College in Texas. He is now a dedicated member of the American Veterinary Chiropractic Association working in conjunction with veterinarians throughout Nevada to provide complete animal care. When asked why Dr. Carter chooses to dedicate his practice to animals he says, "Because animals don't lie and they are able to respond quickly and successfully to treatment."</p>
 					</div>
--- partials/nav-bar.php
+++ partials/nav-bar.php
@@ -4,6 +4,20 @@
 
     <div id="header-wrap">
 
+        <div class="clearfix">
+
+            <table class="lvac-header-table" width="100%" align="center" cellpadding="0" cellspacing="0">
+        		<tbody>
+            		<tr>
+            			<td class="lvac-header-table-side" width="*" background="images/lvac/header_l.jpg">&nbsp;</td>
+            			<td class="lvac-header-table-content" background="images/lvac/header.png"></td>
+            			<td class="lvac-header-table-side" width="*" background="images/lvac/header_r.jpg">&nbsp;</td>
+            		</tr>
+            	</tbody>
+            </table>
+
+        </div>
+
         <div class="container clearfix">
 
             <div id="primary-menu-trigger"><i class="icon-reorder"></i></div>
@@ -11,14 +25,13 @@
             <!-- Logo
             ============================================= -->
             <div id="logo">
-                <a href="index.html" class="standard-logo" data-dark-logo="images/lvac/avac.png"><img src="images/lvac/avca.jpg" alt="Las Vegas Animal Chiro Logo"></a>
-                <a href="index.html" class="retina-logo" data-dark-logo="images/lvac/avac.png"><img src="images/lvac/avca.jpg" alt="Las Vegas Animal Chiro Logo"></a>
-            </div><!-- #logo end -->
+                <!-- <a href="index.html" class="standard-logo" data-dark-logo="images/lvac/avac.png"><img src="images/lvac/avca.jpg" alt="Las Vegas Animal Chiro Logo"></a>
+                <a href="index.html" class="retina-logo" data-dark-logo="images/lvac/avac.png"><img src="images/lvac/avca.jpg" alt="Las Vegas Animal Chiro Logo"></a> -->
+            </div>
 
             <!-- Primary Navigation
             ============================================= -->
             <nav id="primary-menu" class="style-2">
-
                 <ul>
                     <?php foreach($pages as $url=>$title):?>
                     <li <?php if($url === $active_page):?>class="current"<?php endif;?>>
@@ -28,8 +41,7 @@
                     </li>
                     <?php endforeach;?>
                 </ul>
-
-            </nav><!-- #primary-menu end -->
+            </nav>
 
         </div>
 
